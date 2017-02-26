fce503a93e780bc3ac98fb152f5ee725ec57e274
infb_Que <petan4ik@gmail.com>
2017-02-27 00:05:40 +0300

    Добавлены константы

---
+++ core/src/ru/maxbat/spiritsoul/util/Constants.java
@@ -0,0 +1,11 @@
+package ru.maxbat.spiritsoul.util;
+
+/**
+ * Created by user on 26.02.2017.
+ */
+public final class Constants {
+    public static final float PPM = 106;
+    public static final float SCALE = 2f;
+    public static final float VIEWPORT_WIDTH = 12;
+    public static final float VIEWPORT_HEIGHT = 10;
+}
