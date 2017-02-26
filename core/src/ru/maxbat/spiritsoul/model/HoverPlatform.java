f9b82018630536a7c2773d30f3c880086802e98c
infb_Que <petan4ik@gmail.com>
2017-02-27 00:07:08 +0300

    Добавлена модель летающей платформы

---
+++ core/src/ru/maxbat/spiritsoul/model/HoverPlatform.java
@@ -0,0 +1,12 @@
+package ru.maxbat.spiritsoul.model;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by user on 26.02.2017.
+ */
+public class HoverPlatform {
+    public HoverPlatform(World world, float v, int i, int i1, float v1, int i2, int i3, int i4) {
+
+    }
+}
