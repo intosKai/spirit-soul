1ca36135f59ea1ba619cbd4878a3311b81cb83ba
infb_Que <petan4ik@gmail.com>
2017-02-27 00:08:29 +0300

    Пустой листенер коллизий

---
+++ core/src/ru/maxbat/spiritsoul/controller/MyContactListener.java
@@ -0,0 +1,32 @@
+package ru.maxbat.spiritsoul.controller;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+/**
+ * Created by user on 26.02.2017.
+ */
+public class MyContactListener implements ContactListener {
+    public MyContactListener(World world) {
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
+
